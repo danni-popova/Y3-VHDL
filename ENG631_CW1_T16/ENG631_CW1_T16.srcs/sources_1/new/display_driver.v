cd49744c4ef530a9d9f32f2838b56d42349b53de
Tom Lawson <tom@tomlawson.uk>
2018-10-12 19:00:18 +0100

    created files for components

---
+++ ENG631_CW1_T16/ENG631_CW1_T16.srcs/sources_1/new/display_driver.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2018 06:58:31 PM
+// Design Name: 
+// Module Name: display_driver
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module display_driver(
+
+    );
+endmodule
---
+++ ENG631_CW1_T16/ENG631_CW1_T16.srcs/sources_1/new/display_driver.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/12/2018 06:59:46 PM
+-- Design Name: 
+-- Module Name: display_driver - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity display_driver is
+--  Port ( );
+end display_driver;
+
+architecture Behavioral of display_driver is
+
+begin
+
+
+end Behavioral;
