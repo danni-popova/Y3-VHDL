92b9d2d70d3b5ef14028773e4116b840d8d12d18
Yordanka Popova <43949046+danni-popova@users.noreply.github.com>
2018-10-23 16:20:14 +0100

    Work 23/10

---
+++ ENG631_CW1_T16/ENG631_CW1_T16.srcs/sim_1/new/ClockDivider_tb.vhd
@@ -0,0 +1,72 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 23.10.2018 16:06:47
+-- Design Name: 
+-- Module Name: ClockDivider_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ClockDivider_tb is
+--  Port ( );
+end ClockDivider_tb;
+
+architecture Behavioral of ClockDivider_tb is
+
+    component ClockDivider 
+    Generic ( MaxCount : integer range 0 to 400000000 );
+    Port ( reset : in STD_LOGIC;
+           clock : in STD_LOGIC;
+           clockOut : out STD_LOGIC );
+    end component;
+
+signal reset : std_logic; 
+signal clock : std_logic; 
+signal clockOut : std_logic; 
+
+signal counter : integer range 0 to 400000000;
+
+begin
+
+reset_proc: process 
+begin
+    reset <= '0';
+    wait for 5 ns; 
+    reset <= '1';
+    wait for 5 ns;
+    reset <= '0';
+end process;
+
+sysClock: process
+begin 
+    clock <= '0';
+    wait for 100ns;
+    clock <= '1';
+    wait for 100ns; 
+end process;
+
+end Behavioral;
---
+++ ENG631_CW1_T16/ENG631_CW1_T16.srcs/sources_1/new/ClockDivider.vhd
@@ -0,0 +1,66 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 23.10.2018 15:30:21
+-- Design Name: 
+-- Module Name: ClockDivider - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ClockDivider is
+    Generic ( MaxCount : integer range 0 to 400000000 );
+    Port ( reset : in STD_LOGIC;
+           clock : in STD_LOGIC;
+           clockOut : out STD_LOGIC );
+end ClockDivider;
+
+architecture Behavioral of ClockDivider is
+
+--signals
+signal counter : integer range 0 to 400000000;
+
+begin
+
+    clockEnable : process (clock, reset)
+    begin
+        if reset = '1' then
+            counter <= 0;
+            clockOut <= '0'; --Check with Neils about this
+        elsif rising_edge(clock) then
+            clockOut <= '0'; --Check with Neils about this
+            if counter < MaxCount - 1 then
+                counter <= counter +1;
+            else
+                counter <= 0;
+                clockOut <= '1';
+            end if;
+        end if;
+        
+            
+    end process clockEnable;
+
+end Behavioral;
