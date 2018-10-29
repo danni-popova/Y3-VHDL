efaf81c41fc1eb96f9ce0cb6b21260d129f583d1
Yordanka Popova <43949046+danni-popova@users.noreply.github.com>
2018-10-29 15:51:19 +0000

    OH MY EFFIN GOD IT WORKS

--- ENG631_CW1_T16/ENG631_CW1_T16.srcs/sources_1/new/MilestoneOne.vhd
+++ ENG631_CW1_T16/ENG631_CW1_T16.srcs/sources_1/new/MilestoneOne.vhd
@@ -19,16 +19,21 @@ architecture Behavioral of MilestoneOne is
     
     -- main counter register
     signal sigCounter : integer range 0 to 40000000;
+    signal sigCounter2 : integer range 0 to 400000;
     
     -- clock enable for demo
     signal sigClockEnable : std_logic; 
+    signal sigClockEnable2 : std_logic; 
     
     -- LED and seven segment output
     signal sigDisplayCount : integer range 0 to 9;
     
-    -- Counter for anodes change 
+    -- Counter for anodes change vhdl 
     signal sigAnodesPattern : std_logic_vector (3 downto 0);
-    signal sigAnodesCounter : integer range 0 to 3; 
+    signal sigAnodesCounter : integer range 0 to 3;
+    
+    type ta is array(0 to 3) of integer range 0 to 9999;
+    signal sigDigitToDisplay: ta := (0, 0, 0, 0);
     
     -- DCM generated component declaration, copied from stub
     component clk_wiz_0 is 
@@ -45,6 +50,37 @@ begin
 
 Demo_DCM : clk_wiz_0 port map ( clk_out1 => sigSystemClock,
     reset => inputReset, clk_in1 => inputClock); 
+    
+    increment_digit_one: Process(sigSystemClock, sigClockEnable, sigDigitToDisplay)
+    begin 
+    if rising_edge(sigSystemClock) then 
+        if sigClockEnable = '1' then
+            if sigDigitToDisplay(0) < 9 then
+                sigDigitToDisplay(0) <= sigDigitToDisplay(0) + 1;
+            else 
+                sigDigitToDisplay(0) <= 0; 
+                if sigDigitToDisplay(1) < 9 then 
+                    sigDigitToDisplay(1)<= sigDigitToDisplay(1) + 1;
+                else 
+                    sigDigitToDisplay(1) <= 0; 
+                    if sigDigitToDisplay(2) < 9 then
+                       sigDigitToDisplay(2)<= sigDigitToDisplay(2) + 1;
+                    else 
+                        sigDigitToDisplay(2) <= 0;
+                        if sigDigitToDisplay(3)< 9 then
+                           sigDigitToDisplay(3)<= sigDigitToDisplay(3) + 1;
+                       else 
+                           sigDigitToDisplay(0) <= 0;
+                           sigDigitToDisplay(1) <= 0;
+                           sigDigitToDisplay(2) <= 0;
+                           sigDigitToDisplay(3) <= 0;
+                       end if;
+                     end if;
+                 end if; 
+            end if;
+        end if;
+    end if;            
+end process increment_digit_one;
     
     clock_divider: Process (inputReset, sigSystemClock, inputFast, sigCounter)
     begin 
@@ -63,15 +99,22 @@ Demo_DCM : clk_wiz_0 port map ( clk_out1 => sigSystemClock,
         end if;
     end process clock_divider;
     
-    anodes_counter: Process (sigSystemClock, sigAnodesCounter)
-    begin 
-    if rising_edge(sigSystemClock)then
-        if sigAnodesCounter < 3 then
-            sigAnodesCounter <= sigAnodesCounter + 1;
-         else sigAnodesCounter <= 0;
-         end if;
-     end if;
-    end process anodes_counter;
+    clock_divider2: Process (inputReset, sigSystemClock, inputFast, sigCounter2)
+        begin 
+            if inputReset = '1' then 
+                sigCounter2 <= 0;
+            elsif rising_edge(sigSystemClock) then 
+                if sigCounter2 >= (400000-1) then 
+                   sigCounter2 <= 0;
+                else 
+                    if inputFast = '1' then 
+                        sigCounter2 <= sigCounter2 + 1000; 
+                    else 
+                        sigCounter2 <= sigCounter2 + 1; 
+                    end if;
+                end if;
+            end if;
+        end process clock_divider2;
     
     signal_gen: Process (inputReset, sigSystemClock, sigCounter)
     begin
@@ -85,6 +128,19 @@ Demo_DCM : clk_wiz_0 port map ( clk_out1 => sigSystemClock,
             end if; 
         end if;
     end process signal_gen;
+    
+    signal_gen2: Process (inputReset, sigSystemClock, sigCounter2)
+        begin
+            if inputReset = '1' then 
+                sigClockEnable2 <= '0';
+            elsif falling_edge(sigSystemClock) then 
+                if sigCounter2 >= (400000-1) then 
+                    sigClockEnable2 <= '1';
+                else 
+                    sigClockEnable2 <= '0';
+                end if; 
+            end if;
+        end process signal_gen2;
     
     display_sequence: process(inputReset, sigSystemClock, sigClockEnable, sigDisplayCount)
     begin 
@@ -101,26 +157,52 @@ Demo_DCM : clk_wiz_0 port map ( clk_out1 => sigSystemClock,
     end if;
     end process display_sequence;
     
-    with sigDisplayCount select 
+    anodes_counter: Process (sigSystemClock, sigAnodesCounter)
+        begin 
+        if rising_edge(sigSystemClock)then
+            if sigClockEnable2 = '1' then
+                if sigAnodesCounter < 3 then
+                    sigAnodesCounter <= sigAnodesCounter + 1;
+                else sigAnodesCounter <= 0;
+                end if;
+            end if;
+         end if;
+        end process anodes_counter;
+    
+    with sigDigitToDisplay(sigAnodesCounter) select
     outputSegmentCathodes <= "0000001" when 0, 
-                             "1001111" when 1, 
-                             "0010010" when 2,
-                             "0000110" when 3,
-                             "1001100" when 4,
-                             "0100100" when 5,
-                             "0100000" when 6,
-                             "0001111" when 7,
-                             "0000000" when 8,
-                             "0000100" when 9,
-                             "0000000" when others;
+                                 "1001111" when 1, 
+                                 "0010010" when 2,
+                                 "0000110" when 3,
+                                 "1001100" when 4,
+                                 "0100100" when 5,
+                                 "0100000" when 6,
+                                 "0001111" when 7,
+                                 "0000000" when 8,
+                                 "0000100" when 9,
+                                 "0000000" when others;
+    
+--    with sigDisplayCount select 
+--    outputSegmentCathodes <= "0000001" when 0, 
+--                             "1001111" when 1, 
+--                             "0010010" when 2,
+--                             "0000110" when 3,
+--                             "1001100" when 4,
+--                             "0100100" when 5,
+--                             "0100000" when 6,
+--                             "0001111" when 7,
+--                             "0000000" when 8,
+--                             "0000100" when 9,
+--                             "0000000" when others;
      
      with sigAnodesCounter select 
-     sigAnodesPattern <= "0001" when 0,
-                         "0010" when 1,
-                         "0100" when 2,
-                         "1000" when 3,
+     sigAnodesPattern <= "1110" when 0,
+                         "1101" when 1,
+                         "1011" when 2,
+                         "0111" when 3,
                          "0000" when others;
-     
+                         
+    -- set anodes to the anodes pattern
      outputSegmentAnodes <= sigAnodesPattern;
      
      --outputSegmentAnodes <= "1111" when inputReset = '1' else "0000";
