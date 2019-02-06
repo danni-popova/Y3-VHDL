a2954182796a64125642417c1e9fdea3923b4da2
Tom Lawson <tom@tomlawson.uk>
2019-02-06 16:34:44 +0000

    Add Comments to the Debouncer

--- ENG631_CW1_T16_Milestone2/ENG631_CW1_T16_Milestone2.srcs/sources_1/new/T16_M2_Debouncer.vhd
+++ ENG631_CW1_T16_Milestone2/ENG631_CW1_T16_Milestone2.srcs/sources_1/new/T16_M2_Debouncer.vhd
@@ -16,29 +16,30 @@ signal lastState : std_logic;
 
 begin
 
-    debounce : process (clock)    
+    debounce : process (clock)
     begin
       if rising_edge(clock) then
           output <= '0';
           lastState <= input;
-          
+
           if input = '1' then
-              if lastState = input then 
-                highCount := highCount + 1;
-              else 
+              if lastState = input then --if the states are different this means there was a bounce, this is what smothes that out
+                highCount := highCount + 1; --High count stores the number of times it has been consectuively high
+              else
                 highCount := 0;
               end if;
           end if;
-          
-          if highCount = 5000000 then
+
+          if highCount = 5000000 then --When it has been high for the selected number of times a pulse is created
             Output <= '1';
           end if;
-          
-          if highCount > 100000000 then 
+
+          --Means that if you hold the button it will repeat the pulse every second.
+          if highCount > 100000000 then --After it reaches this number it wraps back to zero.
             highCount := 0;
           end if;
       end if;
-      
+
     end process debounce;
 
 end Behavioral;
